5cc82e5990348072d6e640991b8aaaf0253108a8
Gregor Smith <gregor_smith@outlook.com>
2016-04-21 07:54:48 +0100

    archive view that actually works surprisingly well

--- muhblog/__main__.py
+++ muhblog/__main__.py
@@ -1,3 +1,6 @@
-from ._muhblog import main
+try:
+    from ._muhblog import main
+except SystemError:
+    from _muhblog import main
 
 main()
--- muhblog/_muhblog.py
+++ muhblog/_muhblog.py
@@ -2,6 +2,7 @@ import os
 import logging
 import pathlib
 import datetime
+import collections
 import configparser
 
 import click
@@ -19,59 +20,75 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite' + DATABASE_PATH.as_uri()[4:]
 db = flask_sqlalchemy.SQLAlchemy(app)
 
 class Entry(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    path = db.Column(db.Text, unique=True, primary_key=True)
     title = db.Column(db.String(100), unique=True)
+    markdown_text = db.Column(db.Text, unique=True)
     date_written = db.Column(db.DateTime, unique=True)
     date_modified = db.Column(db.DateTime)
-    markdown_text = db.Column(db.Text, unique=True)
-    path = db.Column(db.Text, unique=True)
 
-    def __init__(self, **kwargs):
-        self.update_from_arguments(**kwargs)
-
-    def update_from_arguments(self, title, markdown_text, date_written,
-                              path=None, date_modified=None):
+    def __init__(self, path, title, markdown_text, date_written, date_modified):
+        self.path = path
         self.title = title
         self.markdown_text = markdown_text
         self.date_written = date_written
-        if path is not None:
-            self.path = path
-        if date_modified is not None:
-            self.date_modified = date_modified
+        self.date_modified = date_modified
 
-    @staticmethod
-    def arguments_from_ini_path(path):
+    @classmethod
+    def from_ini_path(cls, path):
         parser = configparser.ConfigParser(interpolation=None)
         parser.read(str(path), encoding='utf-8')
         entry = parser['entry']
 
-        return {'title': entry['title'], 'markdown_text': entry['text'], 'path': str(path),
-                'date_written': datetime.datetime.strptime(entry['date'], DATE_FORMAT),
-                'date_modified': datetime.datetime.fromtimestamp(path.stat().st_mtime)}
+        return cls(path=str(path),
+                   title=entry['title'],
+                   markdown_text=entry['text'],
+                   date_written=datetime.datetime.strptime(entry['date'], DATE_FORMAT),
+                   date_modified=datetime.datetime.fromtimestamp(path.stat().st_mtime))
+
+    def update_from_other(self, other):
+        self.path = other.path
+        self.title = other.title
+        self.markdown_text = other.markdown_text
+        self.date_written = other.date_written
+        self.date_modified = other.date_modified
 
     def html_text(self):
         return markdown.markdown(self.markdown_text)
 
-@app.route('/')
-def entry():
-    return flask.render_template('entry.html')
+    def __repr__(self):
+        return '{}(path={!r})'.format(type(self).__name__, self.path)
+
+@app.route('/archive')
+@app.route('/<year>')
+@app.route('/<year>/<month>')
+@app.route('/<year>/<month>/<day>')
+def archive(**kwargs):
+    entries = collections.defaultdict(lambda: collections.defaultdict(list))
+
+    conditions = [db.extract(attribute, Entry.date_written) == kwargs[attribute]
+                  for attribute in {'year', 'month', 'day'} if attribute in kwargs]
+    for entry in Entry.query.filter(*conditions) if conditions else Entry.query:
+        month = '{:0>2}'.format(entry.date_written.month)
+        entries[str(entry.date_written.year)][month].append(entry)
+
+    return flask.render_template('archive.html', entries_dict=entries)
 
 def reload_database(archive_path):
     db.create_all()
 
     for ini_path in pathlib.Path(archive_path).glob('*.ini'):
-        arguments = Entry.arguments_from_ini_path(ini_path)
-        existing_entry = Entry.query.filter_by(path=str(ini_path)).first()
+        new_entry = Entry.from_ini_path(ini_path)
+        existing_entry = Entry.query.get(str(ini_path))
         if existing_entry is None:
             logging.debug('no entry found for path, adding new: %s', ini_path)
-            db.session.add(Entry(**arguments))
-        elif arguments['date_modified'] > existing_entry.date_modified:
+            db.session.add(new_entry)
+        elif new_entry.date_modified > existing_entry.date_modified:
             logging.debug('entry for path has been modified, updating: %s', ini_path)
-            existing_entry.update_from_arguments(**arguments)
+            existing_entry.update_from_other(new_entry)
         else:
             logging.debug('entry found for path, has not been modified: %s', ini_path)
 
-    for entry in Entry.query.all():
+    for entry in Entry.query:
         if not os.path.exists(entry.path):
             logging.debug('removing entry with nonexistent path from database: %s', entry.path)
             db.session.delete(entry)
@@ -88,8 +105,11 @@ def main(host, port, debug, archive):
     if archive is None:
         raise click.BadParameter("either '--archive' must be provided "
                                  "or the 'BLOG_ARCHIVE_PATH' environment variable must be set")
+
     logging.basicConfig(format='[%(asctime)s %(levelname)s] %(message)s',
                         level=logging.DEBUG if debug else logging.INFO)
 
     CONFIG_DIRECTORY.mkdir(parents=True, exist_ok=True)
     reload_database(archive)
+
+    app.run(host=host, port=port, debug=debug)
