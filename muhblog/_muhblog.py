416d47e803543e4799e2aeafb631256d17e99177
Gregor Smith <gregor_smith@outlook.com>
2016-04-22 22:05:37 +0100

    fuck databases

--- muhblog/_muhblog.py
+++ muhblog/_muhblog.py
@@ -2,60 +2,50 @@ import re
 import logging
 import pathlib
 import datetime
+import functools
 import configparser
 
 import click
 import flask
 import slugify
 import markdown
-import flask_sqlalchemy
 
 DATE_FORMAT = '%Y-%m-%d %H:%M'
-CONFIG_DIRECTORY = pathlib.Path(click.get_app_dir('muhblog'))
-DATABASE_PATH = CONFIG_DIRECTORY / 'muhblog.db'
 MAX_TITLE_LENGTH = 100
+MAX_CACHE_SIZE = 128
 
 app = flask.Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite' + DATABASE_PATH.as_uri()[4:]
 
-db = flask_sqlalchemy.SQLAlchemy(app)
-
-class Entry(db.Model):
-    path = db.Column(db.Text, unique=True, primary_key=True)
-    title = db.Column(db.String(MAX_TITLE_LENGTH), unique=True)
-    markdown_text = db.Column(db.Text, unique=True)
-    date_written = db.Column(db.DateTime, unique=True)
-    date_modified = db.Column(db.DateTime)
-    title_slug = db.Column(db.String(MAX_TITLE_LENGTH))
-    is_hidden = db.Column(db.Boolean)
-
+class Entry:
     formatting_regex = re.compile(r'\[([a-z]+)(?: (.+?))?\](.+?)\[/([a-z]+)\]', re.DOTALL)
 
-    def __init__(self, path, title, title_slug, markdown_text,
-                 date_written, date_modified, is_hidden):
+    def __init__(self, path):
+        self.parser = configparser.ConfigParser(interpolation=None)
         self.path = path
-        self.title = title
-        self.title_slug = title_slug
-        self.markdown_text = markdown_text
-        self.date_written = date_written
-        self.date_modified = date_modified
-        self.is_hidden = is_hidden
+        self.reload()
 
-    @classmethod
-    def from_ini_path(cls, path):
-        parser = configparser.ConfigParser(interpolation=None)
-        parser.read(str(path), encoding='utf-8')
+    def __repr__(self):
+        return '{}(path={!r})'.format(type(self).__name__, self.path)
 
-        entry = parser['entry']
-        title = entry['title']
+    def reload(self):
+        self.parser.clear()
+        self.parser.read(str(self.path), encoding='utf-8')
 
-        return cls(path=str(path),
-                   title=title,
-                   title_slug=slugify.slugify(title, max_length=MAX_TITLE_LENGTH),
-                   markdown_text=entry['text'],
-                   date_written=datetime.datetime.strptime(entry['date'], DATE_FORMAT),
-                   date_modified=datetime.datetime.fromtimestamp(path.stat().st_mtime),
-                   is_hidden=parser.getboolean('entry', 'is_hidden', fallback=True))
+        entry = self.parser['entry']
+
+        self.title = entry['title']
+        self.markdown_text = entry['text']
+        self.datetime_written = datetime.datetime.strptime(entry['date'], DATE_FORMAT)
+        self.timestamp_modified = self.path.stat().st_mtime
+        self.is_hidden = self.parser.getboolean('entry', 'is_hidden', fallback=True)
+
+    @staticmethod
+    @functools.lru_cache(MAX_CACHE_SIZE)
+    def _slugify(text):
+        return slugify.slugify(text, max_length=MAX_TITLE_LENGTH)
+
+    def title_slug(self):
+        return self._slugify(self.title)
 
     @staticmethod
     def formatting_replacer(match):
@@ -66,46 +56,76 @@ class Entry(db.Model):
             return replacement or ''
         return '<span class="spoiler">{}</span>'.format(text)
 
+    @classmethod
+    @functools.lru_cache(MAX_CACHE_SIZE)
+    def _format_text(cls, text):
+        return cls.formatting_regex.sub(cls.formatting_replacer, text)
+
     def formatted_text(self):
-        return self.formatting_regex.sub(self.formatting_replacer, self.markdown_text)
+        return self._format_text(self.markdown_text)
+
+    @staticmethod
+    @functools.lru_cache(MAX_CACHE_SIZE)
+    def _markdown(text):
+        return flask.Markup(markdown.markdown(text))
 
     def html_text(self):
-        return flask.Markup(markdown.markdown(self.formatted_text()))
+        return self._markdown(self.formatted_text())
 
-    def update_from_other(self, other):
-        self.path = other.path
-        self.title = other.title
-        self.title_slug = other.title_slug
-        self.markdown_text = other.markdown_text
-        self.date_written = other.date_written
-        self.date_modified = other.date_modified
-        self.is_hidden = other.is_hidden
+class Archive(dict):
+    def __init__(self, path, show_hidden):
+        super().__init__()
+        self.path = pathlib.Path(path)
+        self.show_hidden = show_hidden
 
-    def __repr__(self):
-        return '{}(path={!r})'.format(type(self).__name__, self.path)
+        self.reload()
 
-def date_conditions(**kwargs):
-    for attribute in ['year', 'month', 'day']:
-        if attribute in kwargs:
-            yield db.extract(attribute, Entry.date_written) == kwargs[attribute]
+    __repr__ = Entry.__repr__
+
+    def reload(self):
+        for path in self.path.glob('*.ini'):
+            if path.is_file():
+                if path in self:
+                    entry = self[path]
+                    if path.stat().st_mtime > entry.timestamp_modified:
+                        entry.reload()
+                else:
+                    self[path] = Entry(path)
+        for path in self.keys():
+            if not path.exists():
+                del self[path]
+
+    def default_conditions(self, **kwargs):
+        if not self.show_hidden:
+            yield lambda entry: not entry.is_hidden
+        for attribute in ['year', 'month', 'day']:
+            if attribute in kwargs:
+                yield lambda entry: getattr(entry.datetime_written, attribute) == kwargs[attribute]
+
+    def filter(self, *additional_conditions, **kwargs):
+        conditions = [*self.default_conditions(**kwargs), *additional_conditions]
+        for entry in self.values():
+            if all(condition(entry) for condition in conditions):
+                yield entry
 
 @app.route('/archive')
 @app.route('/<int:year>')
 @app.route('/<int:year>/<int:month>')
 @app.route('/<int:year>/<int:month>/<int:day>')
 def archive(**kwargs):
-    conditions = list(date_conditions(**kwargs))
-    if not app.show_hidden:
-        conditions.append(Entry.is_hidden.is_(False))
-    query = Entry.query.filter(*conditions).order_by(Entry.date_written.desc())
-    return flask.render_template('archive.html', title='Archive', entries=query)
+    entries = list(app.archive.filter(**kwargs))
+    if not entries:
+        flask.abort(404)
+    entries.sort(key=lambda entry: entry.datetime_written, reverse=True)
+    return flask.render_template('archive.html', title='Archive', entries=entries)
 
 @app.route('/<int:year>/<int:month>/<int:day>/<title_slug>')
 def entry(title_slug, **kwargs):
-    conditions = [*date_conditions(**kwargs), Entry.title_slug == title_slug]
-    if not app.show_hidden:
-        conditions.append(Entry.is_hidden.is_(False))
-    entry = Entry.query.filter(*conditions).first_or_404()
+    entries = app.archive.filter(lambda entry: entry.title_slug() == title_slug, **kwargs)
+    try:
+        entry = next(entries)
+    except StopIteration:
+        flask.abort(404)
     return flask.render_template('entry.html', title=entry.title, entry=entry)
 
 @app.route('/robots.txt')
@@ -125,46 +145,22 @@ def error_403(error):
 def error_500(error):
     return render_error_template(500, 'Internal Server Error', '/static/500.jpg')
 
-def reload_database(archive_path):
-    db.create_all()
-
-    for ini_path in pathlib.Path(archive_path).glob('*.ini'):
-        new_entry = Entry.from_ini_path(ini_path)
-        existing_entry = Entry.query.get(str(ini_path))
-        if existing_entry is None:
-            logging.debug('no entry found for path, adding new: %s', ini_path)
-            db.session.add(new_entry)
-        elif new_entry.date_modified > existing_entry.date_modified:
-            logging.debug('entry for path has been modified, updating: %s', ini_path)
-            existing_entry.update_from_other(new_entry)
-        else:
-            logging.debug('entry found for path, has not been modified: %s', ini_path)
-
-    for entry in Entry.query:
-        if not pathlib.Path(entry.path).exists():
-            logging.debug('removing entry with nonexistent path from database: %s', entry.path)
-            db.session.delete(entry)
-
-    db.session.commit()
-
 @click.command()
-@click.option('--archive', envvar='BLOG_ARCHIVE_PATH',
+@click.option('--archive-path', envvar='BLOG_ARCHIVE_PATH',
               type=click.Path(file_okay=False, writable=True))
 @click.option('--host')
 @click.option('--port', type=int, default=9001, show_default=True)
 @click.option('--debug', is_flag=True, show_default=True)
 @click.option('--show-hidden', is_flag=True, show_default=True)
-def main(archive, host, port, debug, show_hidden):
-    if archive is None:
+def main(archive_path, host, port, debug, show_hidden):
+    if archive_path is None:
         raise click.BadParameter("either '--archive' must be provided "
                                  "or the 'BLOG_ARCHIVE_PATH' environment variable must be set")
 
     logging.basicConfig(format='[%(asctime)s %(levelname)s] %(message)s',
                         level=logging.DEBUG if debug else logging.INFO)
 
-    CONFIG_DIRECTORY.mkdir(parents=True, exist_ok=True)
-    reload_database(archive)
+    app.archive = Archive(archive_path, show_hidden)
 
     app.jinja_env.trim_blocks = app.jinja_env.lstrip_blocks = True
-    app.show_hidden = show_hidden
     app.run(host=host, port=port, debug=debug)
