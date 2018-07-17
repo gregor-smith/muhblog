eb896d0307fbc73921bd05045ce3d818ad92a76f
Gregor Smith <gregor_smith@outlook.com>
2018-07-17 16:21:02 +0100

    webpack, reboot, scss

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+const CSS_FILENAME = 'stylesheet.css'
+
+
+module.exports = {
+    entry: './style/main.scss',
+    output: {
+        path: path.join(__dirname, 'muhblog', 'static'),
+        filename: CSS_FILENAME,
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    use: [
+                        {loader: 'css-loader'},
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                plugins: () => [
+                                    require('precss'),
+                                    require('autoprefixer')
+                                ]
+                            }
+                        },
+                        {loader: 'sass-loader'}
+                    ]
+                })
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin(CSS_FILENAME)
+    ]
+}
