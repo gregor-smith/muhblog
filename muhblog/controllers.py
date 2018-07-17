fa73f03c2b1a657df24b256ed3c55e31e498945a
Gregor Smith <gregor_smith@outlook.com>
2018-07-17 13:39:28 +0100

    split up archive controller, use type annotations

--- muhblog/controllers.py
+++ muhblog/controllers.py
@@ -1,110 +1,128 @@
-import collections
+from typing import Iterable, DefaultDict, List
+from collections import defaultdict
 
-import flask
+from flask import Blueprint, Response, abort
 
-from .models import Entry, TagDefinition, TagMapping, AboutPage
-from .utils import send_configurable_file, Paginator, render_template
+from .models import Entry, Tag, EntryTag, AboutPage
+from .utils import Paginator, template_response
 
-blueprint = flask.Blueprint(
+
+blueprint = Blueprint(
     name='controllers',
     import_name=__name__,
     static_folder='static',
-    template_folder='templates'
+    template_folder='views'
 )
 
 
 @blueprint.route('/', defaults={'page': 1})
 @blueprint.route('/page/<int:page>/')
-def front(page):
+def front(page: int) -> Response:
     entries = Entry.select() \
         .order_by(Entry.date.desc())
     if not entries.count():
-        flask.abort(404)
-    return render_template('front.html', title=None,
-                           paginator=Paginator(entries, page))
+        abort(404)
+    return template_response(
+        'front.html',
+        title=None,
+        paginator=Paginator(query=entries, current_page=page)
+    )
 
 
-@blueprint.route('/archive/')
-@blueprint.route('/<year>/')
-@blueprint.route('/<year>/<month>/')
-@blueprint.route('/<year>/<month>/<day>/')
-@blueprint.route('/tag/<slug>/')
-def archive(year=None, month=None, day=None, slug=None):
-    if slug is not None:
-        entries = Entry.select() \
-            .join(TagMapping,
-                  on=Entry.id == TagMapping.entry_id) \
-            .join(TagDefinition,
-                  on=TagMapping.definition_id == TagDefinition.id) \
-            .where(TagDefinition.slug == slug)
-        title = TagDefinition.get(slug=slug) \
-            .name
-    else:
-        entries = Entry.select()
-        title = 'archive'
-        if year is not None:
-            try:
-                filtr = Entry.date.year == int(year)
-            except ValueError:
-                flask.abort(404)
-            title = year
-            if month is not None:
-                filtr &= Entry.date.month == int(month)
-                title = f'{month}/{year}'
-                if day is not None:
-                    filtr &= Entry.date.day == int(day)
-                    title = f'{day}/{month}/{year}'
-            entries = Entry.select() \
-                .where(filtr)
-
-    groups = collections.defaultdict(
-        lambda: collections.defaultdict(
-            lambda: collections.defaultdict(list)
+def _archive_response(entries: Iterable[Entry], title: str) -> Response:
+    groups: DefaultDict[int, DefaultDict[int, DefaultDict[int, List[Entry]]]] = defaultdict(
+        lambda: defaultdict(
+            lambda: defaultdict(list)
         )
     )
     for entry in entries:
         groups[entry.date.year][entry.date.month][entry.date.day].append(entry)
-
     if not groups:
-        flask.abort(404)
-    return render_template('archive.html', title=title, entries=groups)
+        abort(404)
+    return template_response('archive.html', title=title, entries=groups)
+
+
+@blueprint.route('/archive/')
+def archive() -> Response:
+    entries: Iterable[Entry] = Entry.select() \
+        .iterator()
+    return _archive_response(entries, title='archive')
+
+
+@blueprint.route('/<year>/')
+def archive_by_year(year: str) -> Response:
+    entries: Iterable[Entry]
+    try:
+        entries = Entry.select() \
+            .where(Entry.date.year == int(year)) \
+            .iterator()
+    except ValueError:
+        abort(404)
+    return _archive_response(entries, title=year)
+
+
+@blueprint.route('/<year>/<month>/')
+def archive_by_month(year: str, month: str) -> Response:
+    entries: Iterable[Entry]
+    try:
+        entries = Entry.select() \
+            .where(Entry.date.year == int(year)) \
+            .where(Entry.date.month == int(month)) \
+            .iterator()
+    except ValueError:
+        abort(404)
+    return _archive_response(entries, title=f'{month}/{year}')
+
+
+@blueprint.route('/<year>/<month>/<day>/')
+def archive_by_day(year: str, month: str, day: str) -> Response:
+    entries: Iterable[Entry]
+    try:
+        entries = Entry.select() \
+            .where(Entry.date.year == int(year)) \
+            .where(Entry.date.month == int(month)) \
+            .where(Entry.date.day == int(day)) \
+            .iterator()
+    except ValueError:
+        abort(404)
+    return _archive_response(entries, title=f'{day}/{month}/{year}')
+
+
+@blueprint.route('/tag/<slug>/')
+def archive_by_tag(slug: str) -> Response:
+    tag_name: str
+    try:
+        tag_name = Tag.get(slug=slug) \
+            .name
+    except Tag.DoesNotExist:
+        abort(404)
+    entries: Iterable[Entry] = Entry.select() \
+        .join(EntryTag, on=Entry.id == EntryTag.entry_id) \
+        .join(Tag, on=EntryTag.definition_id == Tag.id) \
+        .where(Tag.slug == slug) \
+        .iterator()
+    return _archive_response(entries, title=tag_name)
 
 
 @blueprint.route('/<year>/<month>/<day>/<slug>/')
-def entry(year, month, day, slug):
+def entry(year: str, month: str, day: str, slug: str) -> Response:
+    entry: Entry
     try:
-        entry = Entry.get(
-            Entry.date.year == int(year),
-            Entry.date.month == int(month),
-            Entry.date.day == int(day),
-            Entry.slug == slug
-        )
+        entry = Entry.select() \
+            .where(Entry.date.year == int(year)) \
+            .where(Entry.date.month == int(month)) \
+            .where(Entry.date.day == int(day)) \
+            .where(Entry.slug == slug) \
+            .get()
     except (ValueError, Entry.DoesNotExist):
-        flask.abort(404)
-    return render_template('entry.html', entry=entry, title=entry.title)
+        abort(404)
+    return template_response('entry.html', title=entry.title, entry=entry)
 
 
 @blueprint.route('/about/')
-def about():
-    return render_template('about.html', title='about', entry=AboutPage.get())
-
-
-@blueprint.route('/stylesheet.css')
-def stylesheet():
-    return send_configurable_file(config_key='BLOG_STYLESHEET_PATH',
-                                  mimetype='text/css',
-                                  fallback='defaults/stylesheet.css')
-
-
-@blueprint.route('/robots.txt')
-def robots_txt():
-    return send_configurable_file(config_key='BLOG_ROBOTS_TXT_PATH',
-                                  mimetype='text/plain',
-                                  fallback='defaults/robots.txt')
-
-
-@blueprint.route('/favicon.png')
-def favicon():
-    return send_configurable_file(config_key='BLOG_FAVICON_PATH',
-                                  mimetype='image/png',
-                                  fallback='defaults/favicon.png')
+def about() -> Response:
+    return template_response(
+        'about.html',
+        title='about',
+        entry=AboutPage.get()
+    )
